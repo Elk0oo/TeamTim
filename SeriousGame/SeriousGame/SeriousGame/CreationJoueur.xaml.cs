1d6f6f83f342c0a09365ed898e4bb1125ecb01ff
remi marciano <remi.marciano@diiage.org>
2018-04-28 00:29:29 +0200

    on peut prendre une photo

--- SeriousGame/SeriousGame/SeriousGame/CreationJoueur.xaml.cs
+++ SeriousGame/SeriousGame/SeriousGame/CreationJoueur.xaml.cs
@@ -3,6 +3,7 @@ using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,51 +13,59 @@ using Xamarin.Forms.Xaml;
 
 namespace SeriousGame
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class CreationJoueur : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreationJoueur : ContentPage
+    {
         Entry entryNom;
         Entry entryPrenom;
         Image imgUser;
         StackLayout stl;
-		public CreationJoueur ()
-		{
-			InitializeComponent ();
-            
-            Label lblNom = new Label() { Text="Nom du joueur" };
+        Button btnTake;
+
+        public CreationJoueur()
+        {
+            InitializeComponent();
+
+            Label lblNom = new Label() { Text = "Nom du joueur" };
             entryNom = new Entry() { Placeholder = "Entrez votre nom" };
             Label lblPrenom = new Label() { Text = "Prénom du joueur" };
             entryPrenom = new Entry() { Placeholder = "Entrez votre prénom" };
-            imgUser = new Image();
-            Button btnTake = new Button() { Text = "Prendre une photo" };
+
+            btnTake = new Button() { Text = "Prendre une photo" };
             btnTake.Clicked += BtnTake_Clicked;
-            stl = new StackLayout() { Children = { lblNom,entryNom ,lblPrenom,entryPrenom,imgUser,btnTake } };
-           
+            stl = new StackLayout() { Children = { lblNom, entryNom, lblPrenom, entryPrenom, btnTake } };
+
             this.Content = stl;
-            
+
         }
 
         private async void BtnTake_Clicked(object sender, EventArgs e)
-        {            
-                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-                {
-                    await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
-                    return;
-                }
-                var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
-                {
-                    Directory = "Sample",
-                    Name = "test.jpg"
-                });
-                if (file == null)
-                    return;
-                await DisplayAlert("File Location", file.Path, "OK");
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Small,
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+            imgUser = new Image();
+            //await DisplayAlert("File Location", file.Path, "OK");
+            imgUser.Source = file.Path;
             imgUser.Source = ImageSource.FromStream(() =>
-                {
-                    var stream = file.GetStream();
-                    file.Dispose();
-                    return stream;
-                });
-            }        
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+            stl.Children.Add(imgUser);
+        }
     }
 }
