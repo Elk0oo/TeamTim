94f78839632c9d1109da418e8df10b9e39f16890
Nicolas <nicol@NICOLAS-PC>
2018-04-28 11:57:42 +0200

    ajout des methodes d'etape

---
+++ SeriousGamev2/SeriousGameAPI/Controllers/QuizzController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SeriousGameAPI.Controllers
+{
+    public class QuizzController : ApiController
+    {
+        [HttpGet]
+        [Route("api/CreateJoueur/{idEpreuve}")]
+        public IHttpActionResult getQuizz(int idEpreuve)
+        {
+            return Ok(getQuizz(idEpreuve));
+        }
+
+        // GET: api/Quizz/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Quizz
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Quizz/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Quizz/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
