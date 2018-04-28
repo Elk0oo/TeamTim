7b6e9f6495036e182cd7cc28b17d683d6501b272
Nicolas <nicol@NICOLAS-PC>
2018-04-28 03:30:30 +0200

    Projet API

--- SeriousGame/SeriousGame/SeriousGame/MainPage.xaml.cs
+++ SeriousGame/SeriousGame/SeriousGame/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -18,10 +20,37 @@ namespace SeriousGame
 
         private void btnValiderCode_Clicked(object sender, EventArgs e)
         {
-            if (txtCodeSalle.Text.ToUpper() == "IRIS ")
+
+            string faceId1 = "";
+            string faceId2 = "";
+            string requestBody = "{\"faceId1\":\"" + faceId1 + "\",\"faceId2\":\"" + faceId2 + "\"}";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/verify%20HTTP/1.1");
+
+            request.ContentType = "application/json";           
+            request.Headers.Add("Ocp-Apim-Subscription-Key", "112d5a31c0724a0e9f459dd2a2d76d53");
+            request.Method = "POST";
+            request.ContentLength = requestBody.Length;
+
+
+
+
+
+            Stream body = request.GetRequestStream();
+            byte[] byteArray = Encoding.UTF8.GetBytes(requestBody);
+    
+            body.Write(byteArray, 0, byteArray.Length);
+       
+
+            HttpWebResponse myResp = ((HttpWebResponse)(request.GetResponse()));
+            var response = request.GetResponse();
+            var reader = new StreamReader(response.GetResponseStream());
+            string content = reader.ReadToEnd();
+            body.Close();
+
+
+            if (txtCodeSalle.Text.ToUpper() == "IRIS")
             {
                 App.Current.MainPage = new CreationJoueur();
-
             }
         }
     }
